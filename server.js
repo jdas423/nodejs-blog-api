5f6fa299bc47db7e647c182c9999670b10cc01f5
jdas423 <jagdishdas.aps@gmail.com>
2024-04-08 21:51:13 +0530

    commited

--- server.js
+++ server.js
@@ -7,8 +7,24 @@ const postRouter=require("./routes/posts/postRoutes")
 const commentRouter=require("./routes/comments/commentRoutes")
 const categoryRouter=require("./routes/categories/categoryRoutes")
 const globalErrorHandler=require("./middlewares/globalErrorHandler")
+const User=require("./model/User/User")
+const appErr=require("./utilis/appErr")
 
 app.use(express.json());
+
+app.use("/",async(req,res,next)=>{
+   try{
+     const user=await User.find();
+     return res.json(
+        {
+            status:"success",
+            data:user
+        }
+     )
+   }catch(err){
+     return next(appErr(err.message, 500));
+   }
+})
 
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/posts",postRouter);
